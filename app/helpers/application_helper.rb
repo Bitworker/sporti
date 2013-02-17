93a1a726bada0fcbe4be787332477c1f59ab3c80
Bitworker Stefan <bitworker.dev@gmail.com>
2013-02-17 17:48:41 +0100

    Styling changes + buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { page_title }
   end
+
+  def is_active_link?(controller_name)
+    "active" if params[:controller] == controller_name
+  end
 end
