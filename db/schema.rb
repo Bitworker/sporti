7ad03c7831924829db2cdb66acc4cb2dfeda2437
Bitworker Stefan <bitworker.dev@gmail.com>
2013-02-23 11:03:51 +0100

    Allways commit the schema rb dude :]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130217204848) do
+ActiveRecord::Schema.define(:version => 20130221204415) do
+
+  create_table "events", :force => true do |t|
+    t.string   "title"
+    t.datetime "starts_at"
+    t.datetime "ends_at"
+    t.boolean  "all_day"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
