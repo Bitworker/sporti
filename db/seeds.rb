68c131697782fb908ed062e5d6ff6d5433f61248
Bitworker Stefan <bitworker.dev@gmail.com>
2013-11-24 18:00:00 +0100

    wording in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-AccountSubscriptionPlan.create(title: 'Kostenlos', price: 0,    :group_amount => 1, :group_size => 5,  :created_at => Time.current, :updated_at => Time.current)
-AccountSubscriptionPlan.create(title: 'Kostenlos', price: 690,  :group_amount => 1, :group_size => 15, :created_at => Time.current, :updated_at => Time.current)
-AccountSubscriptionPlan.create(title: 'Kostenlos', price: 1390, :group_amount => 2, :group_size => 30, :created_at => Time.current, :updated_at => Time.current)
-AccountSubscriptionPlan.create(title: 'Kostenlos', price: 1990, :group_amount => 3, :group_size => 45, :created_at => Time.current, :updated_at => Time.current)
+AccountSubscriptionPlan.create(title: 'Kostenlos',      price: 0,    :group_amount => 1, :group_size => 5,  :created_at => Time.current, :updated_at => Time.current)
+AccountSubscriptionPlan.create(title: 'Starter Paket',  price: 690,  :group_amount => 1, :group_size => 15, :created_at => Time.current, :updated_at => Time.current)
+AccountSubscriptionPlan.create(title: 'Standard Paket', price: 1390, :group_amount => 2, :group_size => 30, :created_at => Time.current, :updated_at => Time.current)
+AccountSubscriptionPlan.create(title: 'Business Paket', price: 1990, :group_amount => 3, :group_size => 45, :created_at => Time.current, :updated_at => Time.current)
