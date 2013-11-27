fb9c9eb84200633b23ee094e6d1d66454ecf43fa
Bitworker Stefan <bitworker.dev@gmail.com>
2013-11-27 12:18:53 +0100

    top Navigation + Font Styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,10 @@ module ApplicationHelper
   def is_active_link?(controller_name)
     "active" if params[:controller] == controller_name
   end
+
+  def google_font_link_tag(family)
+    tag("link", {:rel  => :stylesheet,
+                 :type => Mime::CSS,
+                 :href => "http://fonts.googleapis.com/css?family=#{family}"}, false, false)
+  end
 end
